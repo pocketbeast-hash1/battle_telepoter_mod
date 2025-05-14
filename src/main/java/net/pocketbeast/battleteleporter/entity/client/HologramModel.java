759c55db8faa4ef94fd47670acea8da376c11c47
pocketbeast-hash1 <godlike.skills420@gmail.com>
2025-05-14 21:36:40 +0300

    recreate hologram model to humanoid model. Add item in hand renderer

--- src/main/java/net/pocketbeast/battleteleporter/entity/client/HologramModel.java
+++ src/main/java/net/pocketbeast/battleteleporter/entity/client/HologramModel.java
@@ -2,16 +2,16 @@ package net.pocketbeast.battleteleporter.entity.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 
-public class HologramModel<T extends Entity> extends HierarchicalModel<T> {
+public class HologramModel<T extends LivingEntity> extends HumanoidModel<T> {
 	private final ModelPart waist;
 	private final ModelPart body;
-	private final ModelPart head;
+	public final ModelPart head;
 	private final ModelPart hat;
 	private final ModelPart rightArm;
 	private final ModelPart rightItem;
@@ -21,56 +21,39 @@ public class HologramModel<T extends Entity> extends HierarchicalModel<T> {
 	private final ModelPart leftLeg;
 
 	public HologramModel(ModelPart root) {
+		super(root.getChild("waist"));
 		this.waist = root.getChild("waist");
 		this.body = this.waist.getChild("body");
-		this.head = this.body.getChild("head");
-		this.hat = this.head.getChild("hat");
-		this.rightArm = this.body.getChild("rightArm");
+		this.head = this.waist.getChild("head");
+		this.hat = this.waist.getChild("hat");
+		this.rightArm = this.waist.getChild("right_arm");
 		this.rightItem = this.rightArm.getChild("rightItem");
-		this.leftArm = this.body.getChild("leftArm");
+		this.leftArm = this.waist.getChild("left_arm");
 		this.leftItem = this.leftArm.getChild("leftItem");
-		this.rightLeg = this.body.getChild("rightLeg");
-		this.leftLeg = this.body.getChild("leftLeg");
+		this.rightLeg = this.waist.getChild("right_leg");
+		this.leftLeg = this.waist.getChild("left_leg");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition waist = partdefinition.addOrReplaceChild("waist", CubeListBuilder.create(), PartPose.offset(0.0F, 12.0F, 0.0F));
-
+		PartDefinition waist = partdefinition.addOrReplaceChild("waist", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
 		PartDefinition body = waist.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -12.0F, 0.0F));
-
-		PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition hat = head.addOrReplaceChild("hat", CubeListBuilder.create().texOffs(32, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.5F)), PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition rightArm = body.addOrReplaceChild("rightArm", CubeListBuilder.create().texOffs(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.0F, 2.0F, 0.0F));
-
+		PartDefinition head = waist.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition hat = waist.addOrReplaceChild("hat", CubeListBuilder.create().texOffs(32, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.5F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition rightArm = waist.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.0F, 2.0F, 0.0F));
 		PartDefinition rightItem = rightArm.addOrReplaceChild("rightItem", CubeListBuilder.create(), PartPose.offset(-1.0F, 7.0F, 1.0F));
-
-		PartDefinition leftArm = body.addOrReplaceChild("leftArm", CubeListBuilder.create().texOffs(40, 16).mirror().addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(5.0F, 2.0F, 0.0F));
-
+		PartDefinition leftArm = waist.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(40, 16).mirror().addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(5.0F, 2.0F, 0.0F));
 		PartDefinition leftItem = leftArm.addOrReplaceChild("leftItem", CubeListBuilder.create(), PartPose.offset(1.0F, 7.0F, 1.0F));
-
-		PartDefinition rightLeg = body.addOrReplaceChild("rightLeg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
-
-		PartDefinition leftLeg = body.addOrReplaceChild("leftLeg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
+		PartDefinition rightLeg = waist.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.9F, 12.0F, 0.0F));
+		PartDefinition leftLeg = waist.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(1.9F, 12.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
 
 	@Override
-	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-	}
-
-	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
 		waist.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-	}
-
-	@Override
-	public ModelPart root() {
-		return waist;
 	}
 }
--- src/main/java/net/pocketbeast/battleteleporter/entity/client/HologramRenderer.java
+++ src/main/java/net/pocketbeast/battleteleporter/entity/client/HologramRenderer.java
@@ -1,14 +1,26 @@
 package net.pocketbeast.battleteleporter.entity.client;
 
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.resources.ResourceLocation;
 import net.pocketbeast.battleteleporter.BattleTeleporterMod;
 import net.pocketbeast.battleteleporter.entity.custom.HologramEntity;
 
-public class HologramRenderer extends MobRenderer<HologramEntity, HologramModel<HologramEntity>> {
+public class HologramRenderer extends HumanoidMobRenderer<HologramEntity, HologramModel<HologramEntity>> {
     public HologramRenderer(EntityRendererProvider.Context pContext) {
-        super(pContext, new HologramModel<>(pContext.bakeLayer(ModModelLayers.HOLOGRAM_LAYER)), 1.0f);
+        this(pContext, new HologramModel<>(pContext.bakeLayer(ModModelLayers.HOLOGRAM_LAYER)), 1.0f);
+    }
+
+    public HologramRenderer(EntityRendererProvider.Context pContext, HologramModel<HologramEntity> pModel, float pShadowRadius) {
+        super(pContext, pModel, pShadowRadius);
+//        this.addLayer(new HumanoidArmorLayer<>(
+//                this,
+//                new HologramModel<>(pContext.bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)),
+//                new HologramModel<>(pContext.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR)),
+//                pContext.getModelManager()
+//        ));
     }
 
     @Override
